998d77a502c021f16ce56da3ef1e73fe28a92078
Bernardo Margulis <bernardo@thismakesmehappy.co>
2025-06-18 00:57:35 -0400

    Add title for steps

--- src/ui/components/Steps.tsx
+++ src/ui/components/Steps.tsx
@@ -21,44 +21,48 @@ const Steps = forwardRef<HTMLDivElement, StepsProps>(
         const removeCustomStep = useSwatchStore((state) => state.removeCustomStep);
         const buildSwatches = useSwatchStore((state) => state.buildSwatches);
         return (
-            <Area
-                id="steps"
-                className={className}
-                ref={ref}
-                style={style}
-                data-testid="steps-area"
-            >
-                <Section id="steps-input" ref={equalStepsRef} data-testid="steps-input-section">
-                    {/* Steps-input content */}
-                    <EqualSteps className="figma-mb-sm" />
-                    <CustomSteps />
-                </Section>
-                <ColumnDivider />
-                <Section id="step-labels" data-testid="step-labels-section">
-                    {/* Step-labels content */}
-                    <div data-testid="equal-steps-badges">
-                        <span className="figma-subtitle">Equal Steps: </span>
-                        {steps.map(step => {
-                            return <Badge key={`step-${step}`} className={"figma-mr-sm"}
-                                          data-testid={`equal-step-badge-${step}`}>{String(step)}</Badge>
-                        })}
-                    </div>
-                    {customSteps.size > 0 && (
-                        <div className="figma-mt-md" data-testid="custom-steps-badges">
-                            <span className="figma-subtitle">Custom Steps: </span>
-                            {Array.from(customSteps).map(step => {
-                                return <Badge key={`custom-${step}`} className={"figma-mr-sm"}
-                                              type="primary" iconRight={"minus"} onClick={() => {
-                                    removeCustomStep(step);
-                                    buildSwatches();
-                                }} data-testid={`custom-step-badge-${step}`}>
-                                    {String(step)}
-                                </Badge>
+            <>
+                <div className={"figma-subtitle"}>How Many Color Steps</div>
+
+                <Area
+                    id="steps"
+                    className={className}
+                    ref={ref}
+                    style={style}
+                    data-testid="steps-area"
+                >
+                    <Section id="steps-input" ref={equalStepsRef} data-testid="steps-input-section">
+                        {/* Steps-input content */}
+                        <EqualSteps className="figma-mb-sm" />
+                        <CustomSteps />
+                    </Section>
+                    <ColumnDivider />
+                    <Section id="step-labels" data-testid="step-labels-section">
+                        {/* Step-labels content */}
+                        <div data-testid="equal-steps-badges">
+                            <span className="figma-subtitle">Equal Steps: </span>
+                            {steps.map(step => {
+                                return <Badge key={`step-${step}`} className={"figma-mr-sm"}
+                                              data-testid={`equal-step-badge-${step}`}>{String(step)}</Badge>
                             })}
                         </div>
-                    )}
-                </Section>
-            </Area>
+                        {customSteps.size > 0 && (
+                            <div className="figma-mt-md" data-testid="custom-steps-badges">
+                                <span className="figma-subtitle">Custom Steps: </span>
+                                {Array.from(customSteps).map(step => {
+                                    return <Badge key={`custom-${step}`} className={"figma-mr-sm"}
+                                                  type="primary" iconRight={"minus"} onClick={() => {
+                                        removeCustomStep(step);
+                                        buildSwatches();
+                                    }} data-testid={`custom-step-badge-${step}`}>
+                                        {String(step)}
+                                    </Badge>
+                                })}
+                            </div>
+                        )}
+                    </Section>
+                </Area>
+            </>
         );
     }
 );
