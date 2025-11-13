24e6db4d9a8b7657ef31a38c67ba8d698cef485c
Bernardo Margulis <bernardo@thismakesmehappy.co>
2025-11-13 15:59:16 -0500

    Remove unnecessary functionality from Swatch component

--- src/ui/components/SwatchesInput/Swatch.tsx
+++ src/ui/components/SwatchesInput/Swatch.tsx
@@ -1,4 +1,3 @@
-import React, {useState, useEffect} from "react";
 import SwatchProps from "../../interfaces/SwatchProps";
 import Chip from "@ui/components/SwatchesInput/Chip";
 import SwatchLabels from "@ui/components/SwatchesInput/SwatchLabels";
@@ -7,37 +6,19 @@ const Swatch = ({
                     color,
                     name,
                     className,
-                    updateSwatch,
-                    id,
                 }: SwatchProps) => {
-    const [swatchColor, setSwatchColor] = useState(color);
-    const [swatchName, setSwatchName] = useState(name);
-    const [hasUpdated, updateHasUpdated] = useState(false);
-
-    useEffect(() => {
-        if (updateSwatch && hasUpdated) {
-            updateSwatch(swatchColor, swatchName, id);
-        } else {
-            updateHasUpdated(true);
-        }
-    }, [swatchColor, swatchName]);
-
-    useEffect(() => {
-        setSwatchColor(color);
-        setSwatchName(name);
-    }, [color, name]);
 
     return (
         <div className={('text-center ') + className} data-testid="swatch-container">
             <div className='d-inline-block' data-testid="swatch-chip-container">
-                <Chip color={swatchColor} className={"figma-border"} width={75}
+                <Chip color={color} className={"figma-border"} width={75}
                       height={75} data-testid="swatch-chip" />
             </div>
             <div className={"separator-md"}></div>
             <div data-testid="swatch-labels-container">
                 <SwatchLabels
-                    swatchColor={swatchColor}
-                    swatchName={swatchName}
+                    swatchColor={color}
+                    swatchName={name}
                 />
             </div>
         </div>
--- src/ui/interfaces/SwatchProps.ts
+++ src/ui/interfaces/SwatchProps.ts
@@ -1,13 +1,8 @@
-import { BaseComponentProps } from "./BaseInterfaces";
+import {BaseComponentProps} from "./BaseInterfaces";
 
 interface SwatchProps extends BaseComponentProps {
     color: string;
     name: string;
-    displayOnly?: boolean;
-    canDelete?: boolean;
-    canPick?: boolean;
-    updateSwatch?: (color: string, name: string, id?: string) => void;
-    onDelete?: (id: string) => void;
 }
 
 export default SwatchProps;
